0e9d997358f844389d6858ffa4a8b555e6453af6
tomltd <tomfreestone@gmail.com>
2015-08-04 15:19:21 +0000

    first exercise

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,17 @@
 $("#main").append("Tom Freestone");
+
+
+var email = "tomfreestone@gmail.com";
+
+var newEmail = email.replace ("tomfreestone", "mickthecoat");
+
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = ("I am Tom and I am AWESOME");
+Var funThoughts = awesomeThoughts.replace ("AWESOME", "fun");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+
+
