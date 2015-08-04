eb9ff3e1026e22c3e87f2ef68cba5cf915ab3f5e
Tom Freestone <tomfreestone@gmail.com>
2015-08-04 19:32:49 +0100

    fixed helper

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,10 +8,10 @@ var newEmail = email.replace ("tomfreestone", "mickthecoat");
 console.log(email);
 console.log(newEmail);
 
-var awesomeThoughts = ("I am Tom and I am AWESOME");
-Var funThoughts = awesomeThoughts.replace ("AWESOME", "fun");
+var awesomeThoughts = (" I am Tom and I am AWESOME");
+var funThoughts = awesomeThoughts.replace ("AWESOME","fun");
 
 console.log(awesomeThoughts);
 console.log(funThoughts);
 
-
+$("#main").append(funThoughts);
