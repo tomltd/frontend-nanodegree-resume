86792399a9072fe4a1faf12ff6aaa1666d1b27b6
Tom Freestone <tomfreestone@gmail.com>
2015-08-05 19:36:55 +0100

    added contact details

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,27 +1,40 @@
-var name = ("Tom Freestone");
+/*var name = ("Tom Freestone");
 var role = ("Front-End Developer");
 
 var formattedName = HTMLheaderName.replace("%data%", name);
-$('#header').append(formattedName);
+$('#header').prepend(formattedName);
 
 var formattedRole = HTMLheaderRole.replace("%data%", role);
-$('#header').append(formattedRole);
+$('#header').prepend(formattedRole); */
 
-/*$("#main").append("Tom Freestone");
+var bio = {
+    "name" : "Tom Freestone",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "+44 07941 244 293",
+        "email": "tomfreestone@gmail.com",
+        "github": "tomltd",
+        "twitter": "@tomltd",
+        "location": "Norwich"
+},
+    "welcomeMessage": "Front-End Developer looking for exciting projects",
+    "skills":
+    [
+        "HTML5", "CSS3", "JS", "Git", "grunt"
+    ],
+    "bioPic": "images/fry.jpg"
+}
 
 
-var email = "tomfreestone@gmail.com";
+var name = bio.name;
+var formattedName = HTMLheaderName.replace("%data%", name);
 
-var newEmail = email.replace ("tomfreestone", "mickthecoat");
+var role = bio.role;
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-console.log(email);
-console.log(newEmail);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
-var awesomeThoughts = (" I am Tom and I am AWESOME");
-var funThoughts = awesomeThoughts.replace ("AWESOME","fun");
-
-console.log(awesomeThoughts);
-console.log(funThoughts);
-
-$("#main").append(funThoughts);
-*/
+var mobile = bio.contacts.mobile;
+var formattedMobile = HTMLmobile.replace("%data%", mobile);
+$("#topContacts").append(formattedMobile);
