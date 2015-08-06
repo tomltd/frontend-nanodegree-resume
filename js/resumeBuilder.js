4bf2fdf923cda707b4a3a347fa65fa7a0dd0876e
Tom Freestone <tomfreestone@gmail.com>
2015-08-06 21:22:01 +0100

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,3 @@
-/*var name = ("Tom Freestone");
-var role = ("Front-End Developer");
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-$('#header').prepend(formattedName);
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$('#header').prepend(formattedRole); */
 
 var bio = {
     "name" : "Tom Freestone",
@@ -25,51 +17,6 @@ var bio = {
     "bioPic": "images/tom.jpg"
 }
 
-
-name = bio.name;
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-role = bio.role;
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-mobile = bio.contacts.mobile;
-var formattedMobile = HTMLmobile.replace("%data%", mobile);
-$("#topContacts").append(formattedMobile);
-
-email = bio.contacts.email;
-var formattedEmail = HTMLemail.replace("%data%", email);
-$("#topContacts").append(formattedEmail);
-
-github = bio.contacts.github;
-var formattedGithub = HTMLgithub.replace("%data%", github);
-$("#topContacts").append(formattedGithub);
-
-twitter = bio.contacts.twitter;
-var formattedTwitter = HTMLtwitter.replace("%data%", twitter);
-$("#topContacts").append(formattedTwitter);
-
-contactLocation = bio.contacts.location;
-var formattedLocation = HTMLlocation.replace("%data%", contactLocation);
-$("#topContacts").append(formattedLocation);
-
-welcomeMessage = bio.welcomeMessage;
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", welcomeMessage);
-$("#header").append(formattedWelcomeMsg);
-
-
-skills = bio.skills;
-var formattedSkills = HTMLskills.replace("%data%", skills);
-$("#header").append(HTMLskillsStart);
-$("#skills").append(formattedSkills);
-
-bioPic = bio.bioPic;
-var formattedBioPic = HTMLbioPic.replace("%data%", bioPic);
-$("#header").prepend(formattedBioPic);
-
-// Next Quiz
 
 var work =
  {
@@ -114,33 +61,14 @@ var education =
         ]    
 }
 
-
-// Add multiple jobs to this
-/*var work = {"uea":{
-            "employer" : "University of East Anglia",
-            "location" : "Norwich",
-},
-"amnesty":{
-    "employer" : "Amnesty International",
-    "location" : "London"
-}
-}*/
-//var work.employer = "University of East Anglia";
-//employer = work.employer;
-//var formattedEmployer = HTMLworkEmployer.replace("%data%", employer);
-$("#workExperience").append(HTMLworkStart);
-//$("#workExperience").append(employer);
-
-var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-//var formattedEmployer = HTMLworkEmployer.replace("%data%", work.uea.employer);
-$(".work-entry:last").append(formattedEmployer);
-// add proper object with different jobs
-
-var formattedLocation = HTMLworkLocation.replace("%data%", work.location);
-$(".work-entry:last").append(formattedLocation);
-
-
-//var education["schoolName"] = "University of East Anglia";
-
-
-$("#education").append(HTMLschoolStart);
+var projects =
+    {
+        "projects": [
+            {
+               "title" : "JavaScript CV",
+                "dates" : "01082015 - 10082015",
+                "description" : "A CV populated with javascript",
+                "images" : "images/tom.jpg"
+                
+            }]
+    }
